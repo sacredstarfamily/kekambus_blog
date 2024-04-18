d048f81f1a6dad737e8256a9c4038d4362be7ae9
caleb swenson <calebswenson@calebs-MacBook-Pro.local>
2024-04-17 21:18:48 -0700

    finished ocmmentsss

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
     posts = db.relationship('Post', back_populates='author')
+    comments = db.relationship('Comment', back_populates='user')
     token = db.Column(db.String, index=True, unique=True)
     token_expiration = db.Column(db.DateTime(timezone=True))
 
@@ -63,6 +64,8 @@ class Post(db.Model):
     # In SQL - user_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     author = db.relationship('User', back_populates='posts')
+    comments = db.relationship('Comment', back_populates='user')
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -81,7 +84,8 @@ class Post(db.Model):
             "title": self.title,
             "body": self.body,
             "dateCreated": self.date_created,
-            "author": self.author.to_dict()
+            "author": self.author.to_dict(),
+            "comments": [comment.to_dict() for comment in self.comments]
         }
     def delete(self):
         db.session.delete(self)
@@ -102,7 +106,8 @@ class Comment(db.Model):
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
-    # user = db.relationship('User',back_populates='comments')
+    user = db.relationship('User',back_populates='comments')
+    post = db.relationship('Post',back_populates='comments')
     
     
     # INSERT INTO
